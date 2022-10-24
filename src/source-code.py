d00bdb4ec80214a67ae2406498e131b454169ff8
adrien <djiengou@ualberta.ca>
2022-10-24 14:38:11 -0600

    commit

--- src/source-code.py
+++ src/source-code.py
@@ -1,12 +1,18 @@
 # This will be our source code
 import sqlite3
-import tkinter as tk
-from tkinter import simpledialog
+import tkinter 
+from tkinter import *
+from tkinter import messagebox
+#from tkinter import simpledialog
 
 connection = None
 cursor = None
 
+root = tkinter.Tk() 
+root.title("welcome")
+root.geometry("200x100")
 
+    
 def connect(path):
     global connection, cursor
 
@@ -14,16 +20,27 @@ def connect(path):
     cursor = connection.cursor()
     return
 
+def startSession():
+    tkinter.messagebox.showinfo("session", "A new listening session has been started")
+
+
+s = Button(root, text = "Start session", command = startSession, activeforeground="red", activebackground="pink", pady=10)
+s.pack(side = "top")
+root.mainloop()
+
+
 def main():
-    path_in = input()
-    path = "./" + path_in
-    connect(path)
-    ROOT = tk.Tk()
-
-    ROOT.withdraw()
-
-    user_type = simpledialog.askstring(title = "Song management", prompt = "Are you a user or an artist")
-    print(user_type)
+    '''
+    #prompt the name of the database
+    path_in = input() 
+    #set the path to the database
+    path = "./" + path_in 
+    #connect to the database
+    connect(path) 
+    '''
     
-if __name__ == "__main__":
-    main()
+    #page.withdraw()
+
+    #user_type = simpledialog.askstring(title = "Song management", prompt = "Are you a user or an artist")
+    #print(user_type)
+    
