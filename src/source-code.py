f60e0c44f90899d8acae80da9c93e4db44b7c2a8
adrien <djiengou@ualberta.ca>
2022-10-24 12:12:13 -0600

    modified:   src/source-code.py

--- src/source-code.py
+++ src/source-code.py
@@ -1,1 +1,9 @@
 # This will be our source code
+import tkinter as tk
+from tkinter import simpledialog
+
+ROOT = tk.TK()
+
+ROOT.withdraw()
+
+user_type = simple.askstring(title = "Song management", prompt = "Are you a user or an artist")
